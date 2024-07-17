fed6377d335f2a8ba3ba3dbd093078810513e9a7
Yash <ybawankar31@gmail.com>
2024-07-18 00:35:29 +0530

    Wrote the code to print hello world

---
+++ day1.cpp
@@ -0,0 +1,12 @@
+// Hello World
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hello world!";
+    cout << "nothing";
+    return 0;
+}
+
+// iostream is used for functionality of input and output
